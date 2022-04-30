06ba2c5bd917262cff90efc01fb87ca34726c09e
djokovicdusan <ddjokovic9@gmail.com>
2022-04-30 11:43:29 +0200

    Documented all TERMIN & TEST operations

--- src/main/java/so/test/UcitajTestSO.java
+++ src/main/java/so/test/UcitajTestSO.java
@@ -10,26 +10,47 @@ import domen.Test;
 import so.OpstaSistemskaOperacija;
 
 /**
- *
+ * System operation that returns single Test, given the key.
+ * 
  * @author Dule Djo
  */
-public class UcitajTestSO extends OpstaSistemskaOperacija{
-    private OpstiDomenskiObjekat generalEntity;
-    
-     @Override
-     public void validate(Object entity) throws Exception {
-        if (!(entity instanceof Test)) {
-            throw new Exception("Invalid entity parameter!");
-        }
-    }
+public class UcitajTestSO extends OpstaSistemskaOperacija {
+	/**
+	 * OpstiDomenskiObjekat object which will store the operation result.
+	 */
+	private OpstiDomenskiObjekat generalEntity;
 
-    @Override
-    public void execute(Object entity) throws Exception {
-        Test test =(Test)entity;
-        generalEntity = databaseBroker.nadji(test,test.getTestId());
-    }
-    
-    public OpstiDomenskiObjekat getResult() {
-        return generalEntity;
-    }
+	/**
+	 * Validates an object before the system operation is executed.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void validate(Object entity) throws Exception {
+		if (!(entity instanceof Test)) {
+			throw new Exception("Invalid entity parameter!");
+		}
+	}
+
+	/**
+	 * Executes the operation after the transaction has started.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void execute(Object entity) throws Exception {
+		Test test = (Test) entity;
+		generalEntity = databaseBroker.nadji(test, test.getTestId());
+	}
+
+	/**
+	 * Returns the operation result.
+	 * 
+	 * @return generalEntity Objects which represents the operation result
+	 */
+	public OpstiDomenskiObjekat getResult() {
+		return generalEntity;
+	}
 }
