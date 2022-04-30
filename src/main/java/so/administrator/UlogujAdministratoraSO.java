f1cb745c2953eb031821df0169a9ebe391b22926
djokovicdusan <ddjokovic9@gmail.com>
2022-04-30 11:12:17 +0200

    Documented ADMIN operations

--- src/main/java/so/administrator/UlogujAdministratoraSO.java
+++ src/main/java/so/administrator/UlogujAdministratoraSO.java
@@ -4,32 +4,54 @@
  * and open the template in the editor.
  */
 package so.administrator;
+
 import domen.*;
 import domen.Administrator;
 import domen.OpstiDomenskiObjekat;
 import so.OpstaSistemskaOperacija;
 
 /**
- *
+ * System operation that logs in the admin.
+ * 
  * @author Dule Djo
  */
-public class UlogujAdministratoraSO extends OpstaSistemskaOperacija{
-    private OpstiDomenskiObjekat generalEntity;
-    
-     @Override
-    public void validate(Object entity) throws Exception {
-        if (!(entity instanceof Administrator)) {
-            throw new Exception("Invalid entity parameter!");
-        }
-    }
+public class UlogujAdministratoraSO extends OpstaSistemskaOperacija {
+	/**
+	 * Admin object which will store the operation result.
+	 */
+	private OpstiDomenskiObjekat generalEntity;
 
-    @Override
-    public void execute(Object entity) throws Exception {
-        Administrator a=(Administrator)entity;
-        generalEntity = databaseBroker.uloguj(a,a.getKorisnickoIme(),a.getLozinka());
-    }
-    
-    public OpstiDomenskiObjekat getResult() {
-        return generalEntity;
-    }
+	/**
+	 * Validates an object before the system operation is executed.
+	 * 
+	 * @param param Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void validate(Object entity) throws Exception {
+		if (!(entity instanceof Administrator)) {
+			throw new Exception("Invalid entity parameter!");
+		}
+	}
+
+	/**
+	 * Executes the operation after the transaction has started.
+	 * 
+	 * @param param Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void execute(Object entity) throws Exception {
+		Administrator a = (Administrator) entity;
+		generalEntity = databaseBroker.uloguj(a, a.getKorisnickoIme(), a.getLozinka());
+	}
+
+	/**
+	 * Returns the operation result.
+	 * 
+	 * @return generalEntity Object which represents the operation result
+	 */
+	public OpstiDomenskiObjekat getResult() {
+		return generalEntity;
+	}
 }
