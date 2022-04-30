d560ffd11aa041f22cf4eb9768f8fe7501c956c0
djokovicdusan <ddjokovic9@gmail.com>
2022-04-30 11:08:40 +0200

    Documented the OpstaSistemskaOperacija class

--- src/main/java/so/OpstaSistemskaOperacija.java
+++ src/main/java/so/OpstaSistemskaOperacija.java
@@ -10,42 +10,84 @@ import storage.database.DatabaseBroker;
 import storage.database.connection.DatabaseConnection;
 
 /**
- *
+ * Generic system operation abstract class that all system operations extend.
+ * 
  * @author Dule Djo
  */
 public abstract class OpstaSistemskaOperacija {
-    
-    protected IDBBroker databaseBroker;
+	/**
+	 * Database broker interface that is used for data manipulation.
+	 */
+	protected IDBBroker databaseBroker;
 
-    public OpstaSistemskaOperacija() {
-        this.databaseBroker = new DatabaseBroker();
-    }
+	/**
+	 * Non-parameterized constructor used for initialization of
+	 * OpstaSistemskaOperacija type objects. Initializes DB broker attribute.
+	 */
+	public OpstaSistemskaOperacija() {
+		this.databaseBroker = new DatabaseBroker();
+	}
 
-    public final void templateExecute(Object entity) throws Exception {
-        try {
-            validate(entity);
-            startTransaction();
-            execute(entity);
-            commitTransaction();
-        } catch (Exception ex) {
-            rollbackTransaction();
-            throw ex;
-        }
-    }
+	/**
+	 * Executes the system operation in a specific order, after validating object
+	 * type, then starting a transaction etc.
+	 * 
+	 * @param param Object that is sent as request argument.
+	 * @throws Exception When the preconditions are not pleased or when there is an
+	 *                   Exception thrown by the DBBroker.
+	 */
+	public final void templateExecute(Object entity) throws Exception {
+		try {
+			validate(entity);
+			startTransaction();
+			execute(entity);
+			commitTransaction();
+		} catch (Exception ex) {
+			rollbackTransaction();
+			throw ex;
+		}
+	}
 
-    protected abstract void validate(Object entity) throws Exception;
+	/**
+	 * Validates an object before the system operation is executed.
+	 * 
+	 * @param param Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	protected abstract void validate(Object entity) throws Exception;
 
-    protected abstract void execute(Object entity) throws Exception;
+	/**
+	 * Executes the operation after the transaction has started.
+	 * 
+	 * @param param Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	protected abstract void execute(Object entity) throws Exception;
 
-    private void startTransaction() throws Exception {
-        DatabaseConnection.getInstance().getConnection().setAutoCommit(false);
-    }
+	/**
+	 * Starts the transaction.
+	 * 
+	 * @throws Exception If there were any errors.
+	 */
+	private void startTransaction() throws Exception {
+		DatabaseConnection.getInstance().getConnection().setAutoCommit(false);
+	}
 
-    private void commitTransaction() throws Exception {
-        DatabaseConnection.getInstance().commit();
-    }
+	/**
+	 * Commits the transaction.
+	 * 
+	 * @throws Exception If there were any errors.
+	 */
+	private void commitTransaction() throws Exception {
+		DatabaseConnection.getInstance().commit();
+	}
 
-    private void rollbackTransaction() throws Exception {
-        DatabaseConnection.getInstance().rollback();
-    }
+	/**
+	 * Rollbacks the transaction.
+	 * 
+	 * @throws Exception If there were any errors.
+	 */
+	private void rollbackTransaction() throws Exception {
+		DatabaseConnection.getInstance().rollback();
+	}
 }
