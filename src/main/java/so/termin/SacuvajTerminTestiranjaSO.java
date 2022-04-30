00f0ae9b5ae26955d93af639d2b224b286cf0315
djokovicdusan <ddjokovic9@gmail.com>
2022-04-30 11:41:03 +0200

    Documented TEST operations

--- src/main/java/so/termin/SacuvajTerminTestiranjaSO.java
+++ src/main/java/so/termin/SacuvajTerminTestiranjaSO.java
@@ -9,26 +9,47 @@ import domen.TerminTestiranja;
 import so.OpstaSistemskaOperacija;
 
 /**
- *
+ * System operation that saves a TerminTestiranja in database.
+ * 
  * @author Dule Djo
  */
-public class SacuvajTerminTestiranjaSO extends OpstaSistemskaOperacija{
-    private Long id;
-    
-     @Override
-     public void validate(Object entity) throws Exception {
-        if (!(entity instanceof TerminTestiranja)) {
-            throw new Exception("Invalid entity parameter!");
-        }
-    }
+public class SacuvajTerminTestiranjaSO extends OpstaSistemskaOperacija {
+	/**
+	 * ID of a saved TerminTestiranja
+	 */
+	private Long id;
 
-    @Override
-    public void execute(Object entity) throws Exception {
-        TerminTestiranja i=(TerminTestiranja)entity;
-        id = databaseBroker.sacuvaj(i);
-    }
-    
-    public Long getId() {
-        return id;
-    }
+	/**
+	 * Validates an object before the system operation is executed.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void validate(Object entity) throws Exception {
+		if (!(entity instanceof TerminTestiranja)) {
+			throw new Exception("Invalid entity parameter!");
+		}
+	}
+
+	/**
+	 * Executes the operation after the transaction has started.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void execute(Object entity) throws Exception {
+		TerminTestiranja i = (TerminTestiranja) entity;
+		id = databaseBroker.sacuvaj(i);
+	}
+
+	/**
+	 * Returns the operation result as a primary key.
+	 * 
+	 * @return id ID which represents the primary key of the saved TerminTestiranja.
+	 */
+	public Long getId() {
+		return id;
+	}
 }
--- src/main/java/so/termin/UcitajTerminTestiranjaSO.java
+++ src/main/java/so/termin/UcitajTerminTestiranjaSO.java
@@ -10,26 +10,47 @@ import domen.TerminTestiranja;
 import so.OpstaSistemskaOperacija;
 
 /**
- *
+ * System operation that returns single TerminTestiranja, given the key.
+ * 
  * @author Dule Djo
  */
 public class UcitajTerminTestiranjaSO extends OpstaSistemskaOperacija {
-    private OpstiDomenskiObjekat generalEntity;
-    
-     @Override
-     public void validate(Object entity) throws Exception {
-        if (!(entity instanceof TerminTestiranja)) {
-            throw new Exception("Invalid entity parameter!");
-        }
-    }
+	/**
+	 * OpstiDomenskiObjekat object which will store the operation result.
+	 */
+	private OpstiDomenskiObjekat generalEntity;
 
-    @Override
-    public void execute(Object entity) throws Exception {
-        TerminTestiranja terminTestiranja =(TerminTestiranja)entity;
-        generalEntity = databaseBroker.nadji(terminTestiranja,terminTestiranja.getTerminTestiranjaId());
-    }
-    
-    public OpstiDomenskiObjekat getResult() {
-        return generalEntity;
-    }
+	/**
+	 * Validates an object before the system operation is executed.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void validate(Object entity) throws Exception {
+		if (!(entity instanceof TerminTestiranja)) {
+			throw new Exception("Invalid entity parameter!");
+		}
+	}
+
+	/**
+	 * Executes the operation after the transaction has started.
+	 * 
+	 * @param entity Object that is sent as request argument.
+	 * @throws Exception If there were any type errors.
+	 */
+	@Override
+	public void execute(Object entity) throws Exception {
+		TerminTestiranja terminTestiranja = (TerminTestiranja) entity;
+		generalEntity = databaseBroker.nadji(terminTestiranja, terminTestiranja.getTerminTestiranjaId());
+	}
+
+	/**
+	 * Returns the operation result.
+	 * 
+	 * @return generalEntity Objects which represents the operation result
+	 */
+	public OpstiDomenskiObjekat getResult() {
+		return generalEntity;
+	}
 }
